dd283d7c83dd1c34530455d948bcaa0c32ea12ad
Jabirv <121808816+Jabirv@users.noreply.github.com>
2023-01-20 10:00:06 +0100

    test lole sourcetree

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/api", (req, res) => {
-  res.json({"users": ["Cedric", "Fabien", "Jabir", "AnissTest"] })  
+  res.json({"users": ["Cedric", "Fabien", "Jabirtest", "AnissTest"] })  
 })
 
 app.listen(5000, () => {console.log("le Server est lancé sur le port 5000") })
